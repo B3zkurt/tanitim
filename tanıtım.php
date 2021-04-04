e7c28b4fb166af29363054aa35da98d178688e3d
B3zkurt <68972804+B3zkurt@users.noreply.github.com>
2021-04-05 01:58:44 +0300

    Add files via upload

---
+++ tanıtım.php
@@ -0,0 +1,298 @@
+<!DOCTYPE html>
+<html lang="tr">
+<body bgcolor="">
+  
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <title> BOZKURT </title>
+  <style type="text/css">
+
+
+    .stretch-card>.card {
+     width: 100%;
+     min-width: 100%
+   }
+
+   body {
+     background-color: #363636
+   }
+
+   .flex {
+     -webkit-box-flex: 1;
+     -ms-flex: 1 1 auto;
+     flex: 1 1 auto
+   }
+
+   @media (max-width:991.98px) {
+     .padding {
+       padding: 1.5rem
+     }
+   }
+
+   @media (max-width:767.98px) {
+     .padding {
+       padding: 1rem
+     }
+   }
+
+   .padding {
+     padding: 3rem
+   }
+
+   .card {
+     box-shadow: none;
+     -webkit-box-shadow: none;
+     -moz-box-shadow: none;
+     -ms-box-shadow: none
+   }
+
+   .card {
+     position: relative;
+     display: flex;
+     flex-direction: column;
+     min-width: 0;
+     word-wrap: break-word;
+     background-color: #fff;
+     background-clip: border-box;
+     border: 1px solid #3da5f;
+     border-radius: 0
+   }
+
+   .card .card-body {
+     padding: 1.25rem 1.75rem
+   }
+
+   .card .card-title {
+     color: #000000;
+     margin-bottom: 0.625rem;
+     text-transform: capitalize;
+     font-size: 0.875rem;
+     font-weight: 500
+   }
+
+   .card .card-description {
+     margin-bottom: .875rem;
+     font-weight: 400;
+     color: #76838f
+   }
+
+   .btn.btn-social-icon {
+     width: 50px;
+     height: 50px;
+     padding: 0
+   }
+
+   .template-demo>.btn {
+     margin-right: 0.5rem !important
+   }
+
+   .template-demo {
+     margin-top: 0.5rem !important
+   }
+
+   .btn.btn-rounded {
+     border-radius: 50px
+   }
+
+   .btn-outline-facebook {
+     border: 1px solid #3b579d;
+     color: #3b579d
+   }
+
+   .btn-outline-facebook:hover {
+     background: #3b579d;
+     color: #ffffff
+   }
+
+   .btn-outline-youtube {
+     border: 1px solid #e52d27;
+     color: #e52d27
+   }
+
+   .btn-outline-twitter {
+     border: 1px solid #2caae1;
+     color: #2caae1
+   }
+
+   .btn-outline-dribbble {
+     border: 1px solid #ea4c89;
+     color: #ea4c89
+   }
+
+   .btn-outline-linkedin {
+     border: 1px solid #0177b5;
+     color: #0177b5
+   }
+
+   .btn-outline-instagram {
+     border: 1px solid #dc4a38;
+     color: #dc4a38
+   }
+
+   .btn-outline-twitter:hover {
+     background: #2caae1;
+     color: #ffffff
+   }
+
+   .btn-outline-linkedin:hover {
+     background: #0177b5;
+     color: #ffffff
+   }
+
+   .btn-outline-youtube:hover {
+     background: #e52d27;
+     color: #ffffff
+   }
+
+   .btn-outline-instagram:hover {
+     background: #e52d27;
+     color: #ffffff
+   }
+
+   .btn-facebook {
+     background: #3b579d;
+     color: #ffffff
+   }
+
+   .btn-youtube {
+     background: #e52d27;
+     color: #ffffff
+   }
+
+   .btn-twitter {
+     background: #2caae1;
+     color: #ffffff
+   }
+
+   .btn-dribbble {
+     background: #ea4c89;
+     color: #ffffff
+   }
+
+   .btn-linkedin {
+     background: #0177b5;
+     color: #ffffff
+   }
+
+   .btn-instagram {
+     background: #dc4a38;
+     color: #ffffff
+   }
+
+   .btn-facebook:hover,
+   .btn-facebook:focus {
+     background: #2d4278;
+     color: #ffffff
+   }
+
+   .btn-youtube:hover,
+   .btn-youtube:focus {
+     background: #c21d17;
+     color: #ffffff
+   }
+
+   .btn-twitter:hover,
+   .btn-twitter:focus {
+     background: #1b8dbf;
+     color: #ffffff
+   }
+
+   .btn-dribbble:hover,
+   .btn-dribbble:focus {
+     background: #e51e6b;
+     color: #ffffff
+   }
+
+   .btn-linkedin:hover,
+   .btn-linkedin:focus {
+     background: ##000000;
+     color: ##000000
+   }
+
+   .btn-instagram:hover,
+   .btn-instagram:focus {
+     background: ##000000;
+     color: ##000000
+   }
+
+ </style>
+</head>
+  <br>
+  <center><img width="200" src=""></center>
+  <br>
+  <h2 style="color: blue;" align="center">HACKED BY BOZKURT</h2>
+  <p style="color: red;" align="center"> Bozkurt Kimdir</b></p>
+  <center><button  data-toggle="modal" data-target="#exampleModal" class="btn btn-primary">Ben kimim? / who am I </button></center>
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Ben kimim?</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          Ben Bozkurt 2014'te Sanal Alem'e Başladım Sosyal Medya Platformu Olan Facebook'a 2014 Yılında Başladım Orda Terör İle Dolandırıcılarla Mücadele Ettim Ardından 2017 Yılında İnstagram'a geçtim Orda Devam Ettim Aynı Şeylere Terör İle Dolandırıcılarla Mücadele Ettim Bu Sırada Kendimi Durmadan Eğittim Sosyal Mühendislik , Oltalama , Brute Force Öğrendim Medyada Bir Çok İcraat'a İmzamı Attım Sonra Kendime Dedimki Kendimi Dahada Geliştirmem Lazım Ardından Web'e Geçtim Orda İlk Eğitim Aldım Kendimi Eğittim Hiç Para Vermedim Eğitim'e İnsan Yeterki Araştırsın Araştırınca Öğrenilmiycek Şey Yoktur Web'e girdiğim İlk 1.Haftamda DDos , Botnet , Sql , Bypass , Brute Force , Xss Öğrendim Ardından Yeterince Eğitimliydim İcraat'a Başladım Her Zamanki Gibi Türk'e Mazluma Karşı olan Tüm Ülkeler Hedefimdi Hepsine Saldırdım Özellikle İsrail Ve Yunanistan Sabah Akşam Saldırdım Pes Etmedim Sonra Öğrendiklerim Arttı Kendi İndexlerimi Yapmaya Falan Başladım Html , Css , Php , JavaScript Öğrendim İndexlerimi Yaptım Bi Süre Sonra Yazılım'a başlamışken Yazılım İle Devam Edim dedim Yazılım Alanına Girdim Yazılım Dillerini Araştırdım En Popüler Olan Dilleri Öğrenmeye Başladım Python , Perl , C # , Java Öğrendim Kendi Toollarımı Yazmaya Başladım Ardından Siber Güvenlik Alanına Girdim Siber Güvenlikte Gene Araştıra Araştıra Eğittim Kendimi Her Linux İndiren Hacker Olmuyor :) Alanında Uzman Olman Gerekiyor Ben Siber Güvenlik Alanında Aldığım Eğitimler "Mobil Program Güvenliği , Pentest , Güvenli Yazılım , KVKK , Temel Kriptoloji , Linux İşletim Sistemleri Güvenliği , Web Uygulama Güvenliği , Siber Güvenlik Analisti Eğitimi , Mobil Güvenlik Ve Sızma Teknikleri , Veritabanı Saldırıları Ve Veritabanı Güvenliği , Siber Olay Tespit Ve Müdahale , İkna'nın Sırları"  Eğitimleri Aldım Yeterince Uzmanlaşdım Siber Güvenlik Alanına Gireli 2 Yıl Oldu 18 Sertifika Sahibi Oldum Kısacası Azimle Çalışan Taş'ı Deler Pes Etmeyin Araştırın Bulun Keşfedin Ve Unutma "ZİRVE TEK KİŞİLİKTİR RÜZGAR SERT ESER YA RÜZGARA MAĞLUP OLCAKSIN YA AYAKTA DURCAKSIN"
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Kapat</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <hr class="bg-light">
+  </div>
+  <br><br>
+  <h3 align="center" style="color: red;">My Social Media Accounts<font color="red"></font></h3>
+
+
+  
+<br><br><br>
+  <center><button type="button" class="btn btn-social-icon btn-github"><i class="fa fa-github"></i></button><a href="https://github.com/b3zkurt/"><input type="button" value="Github" /></a>&nbsp;<button type="button" class="btn btn-social-icon btn-google"><i class="fa fa-google"></i></button><a href="https://bozkurthub.blogspot.com/"><input type="button" value="Blog Site" /></a>&nbsp;<button type="button" class="btn btn-social-icon btn-instagram"><i class="fa fa-instagram"></i></button>
+
+  <a href="https://instagram.com/b3zkurt/"><input type="button" value="İnstagram" /></a>
+  <body oncontextmenu="return false" ondragstart="return false" onselectstart="return false">
+  </body>
+
+  <body oncontextmenu="return false" onselectstart="return false" ondragstart="return false"></body>
+  <script type="text/javascript">
+
+
+  var omitformtags=["input", "textarea", "select"]
+ 
+  omitformtags=omitformtags.join("|")
+
+  function disableselect(e){
+  if (omitformtags.indexOf(e.target.tagName.toLowerCase())==-1)
+  return false
+  }
+
+  function reEnable(){
+  return true
+  }
+
+  if (typeof document.onselectstart!="undefined")
+  document.onselectstart=new Function ("return false")
+  else{
+  document.onmousedown=disableselect
+  document.onmouseup=reEnable
+  }
+
+  </script>
+  <body onkeydown="return false">
+
+
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
